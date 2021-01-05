92fc94c7d8b651b06de232198b667d468c322ec3
Mai Nguyen <chimai.dk@gmail.com>
2021-01-05 18:26:07 +1100

    changing all the code - processing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-let board = [['a', 'b', 'c'],
-              ['d', 'e', 'f'],
-              ['g', 'h', 'i']];
+let board = [];
 
 const tick = {
   nought: function (row, column) {
@@ -14,13 +12,7 @@ const tick = {
   }
 }
 
-const clearBoard = function () {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      $(`#${i}${j}`).text('');
-    }
-  }
-}
+const
 
 
 const checkWinner = function () {
@@ -58,66 +50,110 @@ const checkWinner = function () {
   }
 };
 
+let turnCount = 0;
+
+let playerCount = 0;
+let playerIndex = 0;
+let currentPlayer = '';
+let hasWon = false;
+
+let winCount1 = 0;
+let winCount2 = 0;
+let startingPlayer = '';
+
+const gameController = {
+  players: {{name: 'player1',
+            winCount: winCount1}, 'player2'}},
+
+  currentPlayer: '',
+
+  option: ['nought', 'cross'],
+
+  clearBoard: function () {
+    board = [
+      ['a', 'b', 'c'],
+      ['d', 'e', 'f'],
+      ['g', 'h', 'i']
+    ];
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        $(`#${i}${j}`).text('');
+      }
+    }
+  },
+
+  gameStart: function (player) {
+
+    currentPlayer = player;
+
+    $('#playAgainButton').addClass('invisible');
+    $('#winnerAnnounce').addClass('invisible');
+    this.clearBoard();
+  },
+
+  playerTurn: function (row, column) {
+
+    if (board[row][column] === 'O' || board[row][column] === 'X') {
+      return;
+    } else {
+      if (currentPlayer === 'player1') {
+        tick.nought(row, column);
+        turnCount += 1;
+        playerCount += 1;
+        $(`${'#player1'}`).addClass('dimmer');
+        $(`${'#player2'}`).removeClass('dimmer');
+      } else if (currentPlayer === 'player2'){
+        tick.cross(row, column);
+        turnCount += 1;
+        playerCount += 1;
+        $(`${'#player2'}`).addClass('dimmer');
+        $(`${'#player1'}`).removeClass('dimmer');
+      }
+
+      let winner = checkWinner();
+
+      if (winner === 'nought') {
+        hasWon = true;
+        winCount1 += 1;
+        $('#winnerAnnounce').text('Player 1 won!');
+        $('#winnerAnnounce').removeClass('invisible');
+        $('#winCountPlayer1').text(winCount1);
+        $('#playAgainButton').removeClass('invisible');
+      } else if (winner === 'cross') {
+        hasWon = true;
+        winCount2 += 1;
+        $('#winnerAnnounce').text('Player 2 won!');
+        $('#winnerAnnounce').removeClass('invisible');
+        $('#winCountPlayer2').text(winCount2);
+        $('#playAgainButton').removeClass('invisible');
+      }
+    }
+
+    if (turnCount === 9) {
+      hasWon = true;
+      $('#winnerAnnounce').text(`It's a tie!`)
+      $('#playAgainButton').removeClass('invisible');
+    }
+  }
+
+
+}
 
 $(document).ready(function () {
-  let turnCount = 0;
-  const players = ['player1', 'player2'];
-
-  let hasWon = false;
   $(`${'#player2'}`).addClass('dimmer');
-  let winCount1 = 0;
-  let winCount2 = 0;
 
 
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       $(`#${i}${j}`).on('click', function () {
-        let playerIndex = turnCount % 2;
+        playerIndex = playerCount % 2;
 
-        const currentPlayer = players[playerIndex];
+        currentPlayer = players[playerIndex];
 
         if (hasWon === false) {
           if (turnCount <= 9) {
-            if (board[i][j] === 'O' || board[i][j] === 'X') {
-              return;
-            } else {
-              if (currentPlayer === 'player1') {
-                tick.nought(i, j);
-                turnCount += 1;
-                $(`${'#player1'}`).addClass('dimmer');
-                $(`${'#player2'}`).removeClass('dimmer');
-              } else if (currentPlayer === 'player2'){
-                tick.cross(i, j);
-                turnCount += 1;
-                $(`${'#player2'}`).addClass('dimmer');
-                $(`${'#player1'}`).removeClass('dimmer');
-              }
-
-              let winner = checkWinner();
-
-              if (winner === 'nought') {
-                hasWon = true;
-                winCount1 += 1;
-                $('#winnerAnnounce').text('Player 1 won!');
-                $('#winnerAnnounce').removeClass('invisible');
-                $('#winCountPlayer1').text(winCount1);
-                $('#playAgainButton').removeClass('invisible');
-              } else if (winner === 'cross') {
-                hasWon = true;
-                winCount2 += 1;
-                $('#winnerAnnounce').text('Player 2 won!');
-                $('#winnerAnnounce').removeClass('invisible');
-                $('#winCountPlayer2').text(winCount2);
-                $('#playAgainButton').removeClass('invisible');
-              }
-            }
-
-            if (turnCount === 9) {
-              hasWon = true;
-              $('#winnerAnnounce').text(`It's a tie!`)
-              $('#playAgainButton').removeClass('invisible');
-            }
-          } else {
+             else {
             return;
           }
         } else {
@@ -137,11 +173,17 @@ $(document).ready(function () {
     $('#playAgainButton').addClass('invisible');
     $('#winnerAnnounce').addClass('invisible');
     clearBoard();
-
-    if (players[playerIndex]) {
-      turnCount = 1;
-    }
     hasWon = false;
+    turnCount = 0;
+    console.log(currentPlayer);
+
+    if (currentPlayer === 'player1') {
+      playerCount = 1;
+
+    } else if (currentPlayer === 'player2') {
+      playerCount = 0;
+    }
+
   })
 })
 
