b098e24b0e2cc3695ca40c91e5bb426830ba0da2
Mai Nguyen <chimai.dk@gmail.com>
2021-01-08 04:29:50 +1100

    done

--- js/mainTesting.js
+++ js/mainTesting.js
@@ -60,66 +60,63 @@ $(document).ready(function () {
       $('.choose-name').addClass('disappear');
       $(`#player-${game.currentFirstPlayer().id}`).text(player1Name + ': ' + player1Symbol);
       $(`#player-${game.currentSecondPlayer().id}`).text(player2Name + ': ' + player2Symbol);
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          $(`#${i}${j}`).on('click', function () {
+            // stop player from clicking in the same spot
+            if (game.hasWon === true) {
+              return;
+
+            } else {
+
+              if (game.board[i][j] === "O" || game.board[i][j] === "X" ) {
+                return;
+
+              } else {
+                  $(`#${i}${j}`).text(game.currentFirstPlayer().symbol);
+
+                  game.performTurn(i, j);
+
+                  setDimmerPlayer();
+            };
+
+                if (game.hasWon === true) {
+                  game.players[0].winCount++;
+                  game.totalGameCount++;
+
+                  $('#winnerAnnounce').text(`Congrats!! ${game.players[0].name} won!!!`);
+                  $('#playAgainButton').removeClass('invisible');
+                  $('#winnerAnnounce').removeClass('invisible');
+
+
+                } else if (game.hasWon === 'tie') {
+                  game.totalGameCount++;
+                  $('#winnerAnnounce').text(`It's a tie!!!`);
+                  $('#playAgainButton').removeClass('invisible');
+                  $('#winnerAnnounce').removeClass('invisible');
+                }
+
+                render();
+              }
+            }
+          )
+        }
+      }
+
+      $('#playAgainButton').on('click', function () {
+
+        $('#playAgainButton').addClass('invisible');
+        $('#winnerAnnounce').addClass('invisible');
+        clearBoardUI();
+
+        game.setupNewGame();
+
+        render();
+        setDimmerPlayer();
+
+      })
     }
   });
-
-
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      $(`#${i}${j}`).on('click', function () {
-        // stop player from clicking in the same spot
-        if (game.hasWon === true) {
-          return;
-
-        } else {
-
-          if (game.board[i][j] === "O" || game.board[i][j] === "X" ) {
-            return;
-
-          } else {
-              $(`#${i}${j}`).text(game.currentFirstPlayer().symbol);
-
-              game.performTurn(i, j);
-
-              setDimmerPlayer();
-        };
-
-            if (game.hasWon === true) {
-              game.players[0].winCount++;
-              game.totalGameCount++;
-
-              $('#winnerAnnounce').text(`Congrats!! ${game.players[0].name} won!!!`);
-              $('#playAgainButton').removeClass('invisible');
-              $('#winnerAnnounce').removeClass('invisible');
-
-
-            } else if (game.hasWon === 'tie') {
-              game.totalGameCount++;
-              $('#winnerAnnounce').text(`It's a tie!!!`);
-              $('#playAgainButton').removeClass('invisible');
-              $('#winnerAnnounce').removeClass('invisible');
-            }
-
-            render();
-          }
-        }
-      )
-    }
-  }
-
-  $('#playAgainButton').on('click', function () {
-
-    $('#playAgainButton').addClass('invisible');
-    $('#winnerAnnounce').addClass('invisible');
-    clearBoardUI();
-
-    game.setupNewGame();
-
-    render();
-    setDimmerPlayer();
-
-  })
-
 
 });
