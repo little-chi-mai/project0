f6975e72905ed9ecb9c44c70cc6dc76a4c004bca
Mai Nguyen <chimai.dk@gmail.com>
2021-01-07 21:09:18 +1100

    Working with DOM

--- js/gameController.js
+++ js/gameController.js
@@ -8,6 +8,8 @@ const gameController = {
     winCount: 0}],
 
   playerTurnCount: 0,
+  tieGameCount: 0,
+  totalGameCount: 0,
 
   currentFirstPlayer: function () {
     return this.players[0];
@@ -18,7 +20,7 @@ const gameController = {
   },
 
   board: [
-    ['O', 'O', 'O'],
+    ['', '', ''],
     ['', '', ''],
     ['', '', '']
   ],
@@ -47,9 +49,7 @@ const gameController = {
     this.board.forEach(function(row) {
       row.forEach(function (item) {
         if (item !== '') {
-          console.log('mai');
           count += 1;
-          console.log(count);
         }
       })
     });
@@ -58,7 +58,6 @@ const gameController = {
   },
 
   newBoard: function () {
-
     this.board = [
       ['', '', ''],
       ['', '', ''],
@@ -67,10 +66,21 @@ const gameController = {
     return this.board;
   },
 
+  // setupFirstGame: function () {
+  //   this.newBoard();
+  // },
+
   setupNewGame: function () {
-    // this.newBoard();
+    this.newBoard();
     this.playerTurnCount = 0;
-    this.setFirstPlayer(this.findWinner());
+
+    if (this.totalGameCount === 0) {
+      this.setFirstPlayer(this.currentFirstPlayer());
+      console.log('First game starts!');
+    } else {
+      this.setFirstPlayer(this.findWinner());
+      console.log('Ready to play again');
+    };
   },
 
   tick: function (row, column) {
@@ -79,31 +89,50 @@ const gameController = {
 
   performTurn: function (row, column) {
     this.tick(row, column);
-    this.findWinner();
-    this.switchPlayer();
+    // this.findWinner();
+
+    if (!this.findWinner()) {
+      this.switchPlayer();  //switch player if no winner yet
+    };
   },
 
 
   findWinner: function () {
     let winner;
+    let firstPlayer = this.currentFirstPlayer();
+    let secondPlayer = this.currentSecondPlayer();
 
     for (let i = 0; i < 3; i++) {
 
-      if (this.board[i][0] === this.board[i][1] & this.board[i][0] === this.board[i][2]
+      if (this.board[i][0] === this.board[i][1] & this.board[i][0] === this.board[i][2] & this.board[i][0] !== ''
 
-      || this.board[0][i] === this.board[1][i] & this.board[0][i] === this.board[2][i]
-
-      || this.board[0][0] === this.board[1][1] & this.board[0][0] === this.board[2][2]
-
-      || this.board[2][0] === this.board[1][1] & this.board[2][0] === this.board[0][2]) {
-        winner = this.currentFirstPlayer();
+      || this.board[0][i] === this.board[1][i] & this.board[0][i] === this.board[2][i] & this.board[0][i] !== ''
+      //
+      || this.board[0][0] === this.board[1][1] & this.board[0][0] === this.board[2][2] & this.board[0][0] !== ''
+      //
+      || this.board[2][0] === this.board[1][1] & this.board[2][0] === this.board[0][2] & this.board[1][1] !== ''
+    ) {
+        console.log('Someone won!');
+        console.log(this.board[i].toString());
+        winner = firstPlayer;
+        this.players[0].winCount++;
+        this.totalGameCount++;
         return winner;
+
       } else {
-        winner = 'tie';
-        return;
+        console.log('No one won yet!');
+        this.totalGameCount++;
+        this.tieGameCount++;
       }
     }
   },
 
+  checkStatus: function () {
+    console.log('First player ' + this.currentFirstPlayer());
+    console.log('Second player ' + this.currentSecondPlayer());
+    console.log('Current playerTurnCount ' + this.playerTurnCount);
+    console.log(this.board);
+    console.log('Winner ' + this.findWinner());
+  }
 
 }
--- js/mainTesting.js
+++ js/mainTesting.js
@@ -1,1 +1,25 @@
 console.log(gameController.players);
+
+$(document).ready(function () {
+
+  gameController.setupNewGame();
+
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      $(`#${i}${j}`).on('click', function () {
+        if (gameController.board[i][j] === "O" || gameController.board[i][j] === "X" ) {
+          return;
+        } else {
+            $(`#${i}${j}`).text(gameController.currentFirstPlayer().symbol);
+
+            gameController.performTurn(i, j);
+            gameController.checkStatus();
+          }
+        }
+      )
+    }
+  }
+
+
+});
