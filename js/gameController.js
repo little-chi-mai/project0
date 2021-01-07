58e28c428423eb6a2ab0d254af56438f9db7add5
Mai Nguyen <chimai.dk@gmail.com>
2021-01-08 01:03:02 +1100

    WIP

--- js/gameController.js
+++ js/gameController.js
@@ -1,4 +1,4 @@
-const gameController = {
+const game = {
   players: [
     {name: 'playerHuman',
     symbol: '',
@@ -7,16 +7,27 @@ const gameController = {
     symbol: '',
     winCount: 0}],
 
+  symbolNames: {'O': 'Nought', 'X': 'Cross'},
+
   playerTurnCount: 0,
   tieGameCount: 0,
   totalGameCount: 0,
+  hasWon: false,
 
   currentFirstPlayer: function () {
     return this.players[0];
   },
 
+  symbolFirstPlayer: function () {
+    return this.symbolNames[this.currentFirstPlayer().symbol];
+  },
+
   currentSecondPlayer: function () {
     return this.players[1];
+  },
+
+  symbolSecondPlayer: function () {
+    return this.symbolNames[this.currentSecondPlayer().symbol];
   },
 
   board: [
@@ -73,66 +84,72 @@ const gameController = {
   setupNewGame: function () {
     this.newBoard();
     this.playerTurnCount = 0;
+    this.hasWon = false;
 
     if (this.totalGameCount === 0) {
       this.setFirstPlayer(this.currentFirstPlayer());
       console.log('First game starts!');
     } else {
-      this.setFirstPlayer(this.findWinner());
+      this.setFirstPlayer(this.currentSecondPlayer());
       console.log('Ready to play again');
     };
   },
 
   tick: function (row, column) {
     this.board[row][column] = this.currentFirstPlayer().symbol;
+    this.playerTurnCount++;
   },
 
   performTurn: function (row, column) {
     this.tick(row, column);
-    // this.findWinner();
+    // this.findWinnerOrTie();
+    let winner = this.findWinnerOrTie();
 
-    if (!this.findWinner()) {
+    if (!winner) {
+      console.log(winner);
       this.switchPlayer();  //switch player if no winner yet
     };
   },
 
 
-  findWinner: function () {
+  findWinnerOrTie: function () {
     let winner;
-    let firstPlayer = this.currentFirstPlayer();
-    let secondPlayer = this.currentSecondPlayer();
+    // let firstPlayer = this.currentFirstPlayer();
+    // let secondPlayer = this.currentSecondPlayer();
+    // let game = this;
 
-    for (let i = 0; i < 3; i++) {
+    if (this.playerTurnCount <= 9) {
+      for (let i = 0; i < 3; i++) {
 
-      if (this.board[i][0] === this.board[i][1] & this.board[i][0] === this.board[i][2] & this.board[i][0] !== ''
+        if (this.board[i][0] === this.board[i][1] & this.board[i][0] === this.board[i][2] & this.board[i][0] !== ''
 
-      || this.board[0][i] === this.board[1][i] & this.board[0][i] === this.board[2][i] & this.board[0][i] !== ''
-      //
-      || this.board[0][0] === this.board[1][1] & this.board[0][0] === this.board[2][2] & this.board[0][0] !== ''
-      //
-      || this.board[2][0] === this.board[1][1] & this.board[2][0] === this.board[0][2] & this.board[1][1] !== ''
-    ) {
-        console.log('Someone won!');
-        console.log(this.board[i].toString());
-        winner = firstPlayer;
-        this.players[0].winCount++;
-        this.totalGameCount++;
-        return winner;
+        || this.board[0][i] === this.board[1][i] & this.board[0][i] === this.board[2][i] & this.board[0][i] !== ''
+        //
+        || this.board[0][0] === this.board[1][1] & this.board[0][0] === this.board[2][2] & this.board[0][0] !== ''
+        //
+        || this.board[2][0] === this.board[1][1] & this.board[2][0] === this.board[0][2] & this.board[1][1] !== '') {
+          console.log('Someone won!');
+          winner = this.currentFirstPlayer().name;
+          this.hasWon = true;
 
-      } else {
-        console.log('No one won yet!');
-        this.totalGameCount++;
-        this.tieGameCount++;
+        } else if (this.playerTurnCount === 9) {
+          console.log('TIE!!!!!!!!!!');
+          this.hasWon = 'tie';
+          winner = 'No one';
+        }
       }
     }
+    return winner;
   },
 
   checkStatus: function () {
-    console.log('First player ' + this.currentFirstPlayer());
-    console.log('Second player ' + this.currentSecondPlayer());
+    console.log('First player ' + this.currentFirstPlayer().name);
+    console.log('Second player ' + this.currentSecondPlayer().name);
     console.log('Current playerTurnCount ' + this.playerTurnCount);
     console.log(this.board);
-    console.log('Winner ' + this.findWinner());
+    console.log('Winner ' + this.findWinnerOrTie());
+    console.log('TotalGameCount: ' + this.totalGameCount);
+    console.log('---------');
   }
 
 }
--- js/mainTesting.js
+++ js/mainTesting.js
@@ -1,25 +1,97 @@
-console.log(gameController.players);
+console.log(game.players);
+
+const clearBoardUI = function () {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      $(`#${i}${j}`).text('');
+    }
+  }
+}
+
 
 $(document).ready(function () {
 
-  gameController.setupNewGame();
+  const render = function () {
+    // $(`#${game.currentFirstPlayer().name}`).text(`${game.currentFirstPlayer().name}: ${game.symbolFirstPlayer()}`);
+    //
+    // $(`#${game.currentSecondPlayer().name}`).text(`${game.currentSecondPlayer().name}: ${game.symbolSecondPlayer()}`);
+    //
+    // $(`#winCountPlayerHuman`).text(`${game.players[playerIndex(playerHuman).winCount]}`);
 
+    for (const player of game.players) {
+      $(`#${player.name}`).text(`${player.name}: ${player.symbol}`);
+      $(`#winCount${player.name}`).text(`${player.winCount}`);
+    }
+
+    console.log('Rendered');
+  };
+
+
+  const setDimmerPlayer = function () {
+    $(`#${game.currentFirstPlayer().name}`).removeClass('dimmer');
+    $(`#${game.currentSecondPlayer().name}`).addClass('dimmer');
+  };
+
+  game.setupNewGame();
+  game.checkStatus();
+
+  setDimmerPlayer();
+
+  render();
 
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       $(`#${i}${j}`).on('click', function () {
-        if (gameController.board[i][j] === "O" || gameController.board[i][j] === "X" ) {
+        // stop player from clicking in the same spot
+        if (game.hasWon === true) {
           return;
-        } else {
-            $(`#${i}${j}`).text(gameController.currentFirstPlayer().symbol);
 
-            gameController.performTurn(i, j);
-            gameController.checkStatus();
+        } else {
+
+          if (game.board[i][j] === "O" || game.board[i][j] === "X" ) {
+            return;
+
+          } else {
+              $(`#${i}${j}`).text(game.currentFirstPlayer().symbol);
+
+              game.performTurn(i, j);
+
+              setDimmerPlayer();
+        };
+
+            if (game.hasWon === true) {
+              game.players[0].winCount++;
+              game.totalGameCount++;
+
+              $('#winnerAnnounce').text(`${game.players[0].name} won!!!`);
+              $('#playAgainButton').removeClass('invisible');
+
+
+            } else if (game.hasWon === 'tie') {
+              game.totalGameCount++;
+              console.log('TIE!')
+              $('#winnerAnnounce').text(`It's a tie!!!`);
+              $('#playAgainButton').removeClass('invisible');
+            }
           }
+          game.checkStatus();
         }
       )
     }
   }
 
+  $('#playAgainButton').on('click', function () {
+
+    $('#playAgainButton').addClass('invisible');
+    $('#winnerAnnounce').addClass('invisible');
+    clearBoardUI();
+
+    game.setupNewGame();
+
+    render();
+    setDimmerPlayer();
+
+
+  })
 
 });
